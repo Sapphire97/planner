2077dfaf75574b278031822febe32ae04d0bb705
comfort.liwambano <comfort.liwambano@wfp.org>
2023-05-16 14:13:54 +0200

    chore: Patch view task dialog to show created on and last updated on dates

--- src/components/ViewTaskDialog/ViewTaskDialog.tsx
+++ src/components/ViewTaskDialog/ViewTaskDialog.tsx
@@ -49,11 +49,19 @@ const ViewTaskDialog = ({closeOverlay}: ViewTaskProps) => {
                         <TextRowLabel htmlFor="input-title">Title</TextRowLabel>
                         <TextRowLabel htmlFor="input-description">Description</TextRowLabel>
                         <TextRowLabel htmlFor="input-dueDate">Due Date</TextRowLabel>
+                        <TextRowLabel htmlFor="input-createdOn">Created On:</TextRowLabel>
+                        <TextRowLabel htmlFor="input-updatedOn">Last Updated On:</TextRowLabel>
                     </TextColumn>
                     <TextColumn>
                         <TextRowContent id="input-title"> {task.title} </TextRowContent>
                         <TextRowContent id="input-description"> {task.description} </TextRowContent>
                         <TextRowContent id="input-dueDate"> {(new Date(task.dueDate)).toDateString()} </TextRowContent>
+                        <TextRowContent id="input-createdOn">
+                            {`${(new Date(task.createdOn)).toDateString()} ${(new Date(task.createdOn)).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}`}
+                        </TextRowContent>
+                        <TextRowContent id="input-updatedOn">
+                            {`${(new Date(task.updatedOn)).toDateString()} ${(new Date(task.updatedOn)).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}`}
+                        </TextRowContent>
                     </TextColumn>
                 </TextContainer>
                 {task.subTasks && task.subTasks.length > 0 && <span style={{textAlign: "left", marginTop: "1em"}}>Checklist</span>}

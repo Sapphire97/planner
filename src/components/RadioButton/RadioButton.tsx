a44fafa6c6e6bc85d342e6aafb88cf63827ddc1a
comfort.liwambano <comfort.liwambano@wfp.org>
2023-05-16 15:28:10 +0200

    chore: Fix view task being triggered when archiving task

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,14 +1,26 @@
+import React from "react";
 import {RadioButtonContainer, RadioInput, RadioLabel} from "./RadioButton-styled";
+
 
 type RadioButtonProps<T> = {
     content?: string,
     value: T,
     onClick: (value: T) => void
 }
+
+type HandleClickProps = {
+    e: React.MouseEvent
+    value: any
+}
 const RadioButton = ({content, value, onClick}: RadioButtonProps<any>) => {
+
+    const handleClick = ({e, value} : HandleClickProps) => {
+        e.stopPropagation()
+        onClick(value)
+    }
     return (
         <RadioButtonContainer id={"radio"} data-testid={"radio-container"}>
-            <RadioInput id={`radio-${value}`} type="radio" value={value} onClick={() => onClick(value)} />
+            <RadioInput id={`radio-${value}`} type="radio" value={value} onClick={(e) => handleClick({e, value})} />
             {content && <RadioLabel htmlFor={`radio-${value}`}> {content} </RadioLabel>}
         </RadioButtonContainer>
     )

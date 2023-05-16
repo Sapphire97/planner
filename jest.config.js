2e9ef04f665c3db74391cefb59f9df175f2c3e7b
Daniel Justin <danieljmuleke@gmail.com>
2023-05-16 15:03:02 +0200

    Configured jest to ignore css imports

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   transform: {
     "^.+\\.tsx?$": "babel-jest"
-  }
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
